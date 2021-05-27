37650c8d3ba46e4e18543f205eac9c6525d47682
Arya-P-Menon <aryas.pmenon@gmail.com>
2021-05-27 15:42:29 +0530

    boot-hello

---
+++ boot-hello/src/main/java/com/ibm/boothhello/bean/Login.java
@@ -0,0 +1,46 @@
+package com.ibm.boothhello.bean;
+
+public class Login {
+
+	private String name;
+	private String password;
+	
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Login(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Login [name=" + name + ", password=" + password + "]";
+	}
+	
+	
+}
